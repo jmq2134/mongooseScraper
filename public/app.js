b321ff2dde71b8e9e492ed50d434283b07ce1490
Julie Quinn <jmq2134@gmail.com>
2017-09-07 13:33:41 -0400

    styling finished

--- public/app.js
+++ public/app.js
@@ -1,9 +1,13 @@
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
+
+  // Number of articles to scrape
+  var numArticles = 20;
+
   // For each one
-  for (var i = 0; i < data.length; i++) {
+  for (var i = 0; i < numArticles; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p><hr>");
   }
 });
 
